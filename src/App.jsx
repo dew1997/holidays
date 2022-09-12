44aea83a29b77aed9c27347a8aa4317a6c2141cf
Dewei <tan.dewei@hotmail.com>
2022-09-12 11:29:15 +0800

    add eslint and rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 function App() {
+  const [msg, setMsg] = useState();
+  fetch("http://localhost:4000/")
+    .then((response) => response.json())
+    .then((data) => setMsg(data.msg));
+
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <h2>{msg}</h2>
     </div>
   );
 }
